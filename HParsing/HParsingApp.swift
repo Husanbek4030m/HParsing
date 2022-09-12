c3ee38d910167046ec5e2c6b57030d71c3be1581
husanbek <husanbek4030m@gmail.com>
2022-09-12 20:27:40 +0500

    Initial Commit

---
+++ HParsing/HParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HParsingApp.swift
+//  HParsing
+//
+//  Created by Karavella on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
